f0832d9aa81bedaeb165735d24c4b9e8c205c601
Jason Hao <jasonhao518@gmail.com>
2024-11-10 13:10:55 +0100

    fix windows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -330,14 +330,17 @@ fn list_apps(app: tauri::AppHandle) -> String {
 
 
 #[tauri::command]
-async fn start_network_disk(models_path: PathBuf, mount_path: PathBuf) {
+async fn start_network_disk(models_path: PathBuf, mount_path: PathBuf, models_data_path: PathBuf) {
     env::set_var("SALT", "ai.ppts.appstore");
     #[cfg(target_os = "windows")]
     {
+        if !models_data_path.exists() {
+            fs::copy(&models_path, &models_data_path)?;
+        }
         let _ = Command::new("juicefs")
             .env("SALT", "ai.ppts.appstore")
             .arg("mount")
-            .arg(format!("sqlite3://{}?_pragma_key=2DD29CA851E7B56E4697B0E1F08507293D761A05CE4D1B628663F411A8086D99&_pragma_cipher_page_size=4096",models_path.display()))
+            .arg(format!("sqlite3://{}?_pragma_key=2DD29CA851E7B56E4697B0E1F08507293D761A05CE4D1B628663F411A8086D99&_pragma_cipher_page_size=4096",models_data_path.display()))
             .arg("Z:")
             .spawn(); // Detached process, ignore any errors or output
     }
@@ -403,12 +406,16 @@ pub fn run() {
                 .path()
                 .resolve("models.db", BaseDirectory::Resource)
                 .unwrap();
+            let models_data_path = app_handle
+                .path()
+                .resolve("models.db", BaseDirectory::AppData)
+                .unwrap();            
             let mount_path = app_handle
                 .path()
                 .resolve("models", BaseDirectory::AppData)
                 .unwrap();
             tauri::async_runtime::spawn(async {
-                start_network_disk(models_path, mount_path).await;
+                start_network_disk(models_path, mount_path,models_data_path).await;
             });
             create_containers_conf(app.handle())?;
             create_env_file(app.handle())?;
