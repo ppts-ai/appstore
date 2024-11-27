1b722a99832211f8a164d98f5a839d9328aa9fa5
Jason Hao <jasonhao518@gmail.com>
2024-11-27 22:14:02 +0100

    Update IntroPage.tsx

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -11,18 +11,11 @@ const IntroPage = () => {
 
     setMessages((prevMessages) => [...prevMessages, "Windows环境，检测WSL虚拟化工具是否已经安装"]);
     const status_command = Command.create('wsl', ["--status"]);
-    //status_command.stdout.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
-    //status_command.stderr.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
+    status_command.stdout.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
+    status_command.stderr.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
     const statusResult = await status_command.execute();
     setMessages((prevMessages) => [...prevMessages, `status result ${JSON.stringify(statusResult)}`]);
-    console.log("wsl status: " + JSON.stringify(statusResult))
-    const wsl_command = Command.create('wsl', ["--install","--no-distribution"]);
-  
-    //wsl_command.stdout.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
-    //wsl_command.stderr.on('data', line => setMessages((prevMessages) => [...prevMessages, line]));
-    const result = await wsl_command.execute();
-    setMessages((prevMessages) => [...prevMessages, `status result ${JSON.stringify(result)}`]);
-    console.log("wsl install result: " + JSON.stringify(result))
+
   };
     return (
       <div>
@@ -31,7 +24,11 @@ const IntroPage = () => {
           So after the application installed, it needs to initialize virtual machine first.
         </div>
         {currentPlatform === "windows" && (
-          <div>on windows, it uses WSL (Windows Subsysem Linux), make sure WSL 2.0 is installed and configured properly </div>
+          <div>on windows, it uses WSL (Windows Subsysem Linux), make sure WSL 2.0 is installed and configured properly 
+          <button onClick={checkWSL}> Check WSL</button>
+          <br />
+          <a target="_blank" href="https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi">Download WSL update here </a>
+          </div>
         ) }
         {currentPlatform === "macos" && (
           <div>mac</div>
