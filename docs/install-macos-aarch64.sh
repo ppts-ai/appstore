ffbd77bf445bc538d1d1f254175a18e79768c0eb
Jason Hao <jasonhao518@gmail.com>
2024-12-07 23:58:37 +0100

    Update install-macos-aarch64.sh

--- docs/install-macos-aarch64.sh
+++ docs/install-macos-aarch64.sh
@@ -46,9 +46,3 @@ echo "Service juicefs setup completed."
 else
   echo "Service creation skipped: key is empty"
 fi
-
-
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo |  sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-sudo rpm-ostree install -y nvidia-container-toolkit
-sudo nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
-sudo nvidia-ctk cdi list
